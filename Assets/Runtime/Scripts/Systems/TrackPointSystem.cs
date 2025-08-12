0fdfe021472dbf492ec51d8ce1186ccad7ef9309
IndividualKex <individualkex@gmail.com>
2025-08-12 01:43:15 -0400

    v0.10.3b

--- Assets/Runtime/Scripts/Systems/TrackPointSystem.cs
+++ Assets/Runtime/Scripts/Systems/TrackPointSystem.cs
@@ -2,16 +2,22 @@ using Unity.Mathematics;
 using Unity.Entities;
 using Unity.Collections;
 using Unity.Burst;
+using Unity.Jobs;
 using static KexEdit.Constants;
 
 namespace KexEdit {
     [UpdateInGroup(typeof(SimulationSystemGroup), OrderLast = true)]
-    [BurstCompile]
     public partial class TrackPointSystem : SystemBase {
         private BufferLookup<Point> _pointLookup;
         private BufferLookup<TrackPoint> _trackPointLookup;
 
         private EntityQuery _query;
+        private EntityQuery _countQuery;
+
+        private struct TrackStyleData {
+            public float Spacing;
+            public int LCM;
+        }
 
         protected override void OnCreate() {
             _pointLookup = SystemAPI.GetBufferLookup<Point>(true);
@@ -21,6 +27,10 @@ namespace KexEdit {
                 .WithAll<SectionReference, TrackPoint, TrackHash, TrackStyle, Segment>()
                 .Build(EntityManager);
 
+            _countQuery = new EntityQueryBuilder(Allocator.Temp)
+                .WithAll<SectionReference, TrackStyle, Segment>()
+                .Build(EntityManager);
+
             RequireForUpdate(_query);
         }
 
@@ -28,96 +38,167 @@ namespace KexEdit {
             _pointLookup.Update(this);
             _trackPointLookup.Update(this);
 
-            int count = _query.CalculateEntityCount();
+            int count = _countQuery.CalculateEntityCount();
             var countMap = new NativeParallelHashMap<Entity, int>(count, Allocator.TempJob);
+            var entities = new NativeArray<Entity>(count, Allocator.TempJob);
+            var sections = new NativeArray<SectionReference>(count, Allocator.TempJob);
+            var styleData = new NativeArray<TrackStyleData>(count, Allocator.TempJob);
+            var segments = new NativeArray<Segment>(count, Allocator.TempJob);
+
+            int index = 0;
             foreach (var (section, style, segment, entity) in SystemAPI
                 .Query<SectionReference, TrackStyle, Segment>()
                 .WithEntityAccess()
             ) {
-                if (!SystemAPI.HasBuffer<Point>(section)) continue;
-                int trackPointCount = CalculateTrackPointCount(section, style, segment);
-                countMap.Add(entity, trackPointCount);
+                entities[index] = entity;
+                sections[index] = section;
+
+                int lcm = 1;
+                foreach (var duplicationMesh in style.DuplicationMeshes) {
+                    lcm = LCM(lcm, duplicationMesh.Step);
+                }
+
+                styleData[index] = new TrackStyleData {
+                    Spacing = style.Spacing,
+                    LCM = lcm
+                };
+
+                segments[index] = segment;
+                index++;
             }
+
+            var countJob = new CalculateCountJob {
+                Entities = entities,
+                Sections = sections,
+                StyleData = styleData,
+                Segments = segments,
+                PointLookup = _pointLookup,
+                CountMap = countMap.AsParallelWriter()
+            };
+
+            var countHandle = countJob.Schedule(count, 32, Dependency);
+
+            using var disposeJobs = new NativeArray<JobHandle>(4, Allocator.Temp) {
+                [0] = entities.Dispose(countHandle),
+                [1] = sections.Dispose(countHandle),
+                [2] = styleData.Dispose(countHandle),
+                [3] = segments.Dispose(countHandle)
+            };
+            var disposalHandle = JobHandle.CombineDependencies(disposeJobs);
 
             Dependency = new BuildJob {
                 PointLookup = _pointLookup,
                 TrackPointLookup = _trackPointLookup,
                 CountMap = countMap,
                 VisualizationMode = VisualizationSystem.CurrentMode
-            }.ScheduleParallel(_query, Dependency);
+            }.ScheduleParallel(_query, countHandle);
 
             Dependency = countMap.Dispose(Dependency);
         }
 
-        private int CalculateTrackPointCount(Entity section, TrackStyle style, Segment segment) {
-            if (!SystemAPI.HasBuffer<Point>(section)) {
-                return 2;
-            }
-
-            var points = SystemAPI.GetBuffer<Point>(section);
-            if (points.Length < 2) {
-                return 2;
-            }
-
-            var (startIndex, endIndex) = GetPointRange(points, segment);
-            if (endIndex <= startIndex) {
-                return 2;
-            }
-
-            float trackLength = points[endIndex].Value.TotalLength - points[startIndex].Value.TotalLength;
-            if (trackLength <= 0f) {
-                return 2;
-            }
-
-            int lcm = 1;
-            foreach (var duplicationMesh in style.DuplicationMeshes) {
-                lcm = LCM(lcm, duplicationMesh.Step);
-            }
-
-            float nominalCount = trackLength / style.Spacing;
-            int baseCount = math.max(2, (int)math.round(nominalCount));
-
-            if (lcm == 1) {
-                return baseCount;
-            }
-
-            return math.max(lcm, (baseCount / lcm) * lcm);
-        }
-
-        private (int startIndex, int endIndex) GetPointRange(DynamicBuffer<Point> points, Segment segment) {
-            if (points.Length < 2) {
-                return (0, 0);
-            }
-
-            int startIndex = 0;
-            int endIndex = points.Length - 1;
-
-            if (segment.StartTime > 0f) {
-                float targetStartTime = segment.StartTime;
-                for (int i = 0; i < points.Length; i++) {
-                    float pointTime = i / HZ;
-                    if (pointTime >= targetStartTime) {
-                        startIndex = i;
-                        break;
-                    }
-                }
-            }
-
-            if (segment.EndTime < float.MaxValue) {
-                float targetEndTime = segment.EndTime;
-                for (int i = startIndex; i < points.Length; i++) {
-                    float pointTime = i / HZ;
-                    if (pointTime >= targetEndTime) {
-                        endIndex = math.max(startIndex + 1, i);
-                        break;
-                    }
-                }
-            }
-
-            return (startIndex, endIndex);
-        }
-
         [BurstCompile]
+        private struct CalculateCountJob : IJobParallelFor {
+            [ReadOnly]
+            public NativeArray<Entity> Entities;
+            [ReadOnly]
+            public NativeArray<SectionReference> Sections;
+            [ReadOnly]
+            public NativeArray<TrackStyleData> StyleData;
+            [ReadOnly]
+            public NativeArray<Segment> Segments;
+            [ReadOnly]
+            public BufferLookup<Point> PointLookup;
+
+            public NativeParallelHashMap<Entity, int>.ParallelWriter CountMap;
+
+            public void Execute(int index) {
+                var entity = Entities[index];
+                var section = Sections[index];
+                var style = StyleData[index];
+                var segment = Segments[index];
+
+                if (!PointLookup.HasBuffer(section)) {
+                    CountMap.TryAdd(entity, 2);
+                    return;
+                }
+
+                var points = PointLookup[section];
+                if (points.Length < 2) {
+                    CountMap.TryAdd(entity, 2);
+                    return;
+                }
+
+                var (startIndex, endIndex) = GetPointRange(points, segment);
+                if (endIndex <= startIndex) {
+                    CountMap.TryAdd(entity, 2);
+                    return;
+                }
+
+                float trackLength = points[endIndex].Value.TotalLength - points[startIndex].Value.TotalLength;
+                if (trackLength <= 0f) {
+                    CountMap.TryAdd(entity, 2);
+                    return;
+                }
+
+                float nominalCount = trackLength / style.Spacing;
+                int baseCount = math.max(2, (int)math.round(nominalCount));
+
+                int finalCount = style.LCM == 1 ? baseCount : math.max(style.LCM, (baseCount / style.LCM) * style.LCM);
+                CountMap.TryAdd(entity, finalCount);
+            }
+
+            private (int startIndex, int endIndex) GetPointRange(DynamicBuffer<Point> points, Segment segment) {
+                if (points.Length < 2) {
+                    return (0, 0);
+                }
+
+                int startIndex = 0;
+                int endIndex = points.Length - 1;
+
+                if (segment.StartTime > 0f) {
+                    float targetStartTime = segment.StartTime;
+                    int left = 0;
+                    int right = points.Length - 1;
+
+                    while (left <= right) {
+                        int mid = left + (right - left) / 2;
+                        float pointTime = mid / HZ;
+
+                        if (pointTime < targetStartTime) {
+                            left = mid + 1;
+                        }
+                        else {
+                            startIndex = mid;
+                            right = mid - 1;
+                        }
+                    }
+                }
+
+                if (segment.EndTime < float.MaxValue) {
+                    float targetEndTime = segment.EndTime;
+                    int left = startIndex;
+                    int right = points.Length - 1;
+
+                    while (left <= right) {
+                        int mid = left + (right - left) / 2;
+                        float pointTime = mid / HZ;
+
+                        if (pointTime < targetEndTime) {
+                            left = mid + 1;
+                        }
+                        else {
+                            endIndex = mid;
+                            right = mid - 1;
+                        }
+                    }
+
+                    endIndex = math.max(startIndex + 1, endIndex);
+                }
+
+                return (startIndex, endIndex);
+            }
+        }
+
         private static int GCD(int a, int b) {
             while (b != 0) {
                 int temp = b;
@@ -127,7 +208,6 @@ namespace KexEdit {
             return a;
         }
 
-        [BurstCompile]
         private static int LCM(int a, int b) {
             return (a * b) / GCD(a, b);
         }
@@ -206,7 +286,7 @@ namespace KexEdit {
                 }
             }
 
-            private (int startIndex, int endIndex) GetPointRange(DynamicBuffer<Point> points, Segment segment) {
+            private static (int startIndex, int endIndex) GetPointRange(DynamicBuffer<Point> points, Segment segment) {
                 if (points.Length < 2) {
                     return (0, 0);
                 }
@@ -216,24 +296,42 @@ namespace KexEdit {
 
                 if (segment.StartTime > 0f) {
                     float targetStartTime = segment.StartTime;
-                    for (int i = 0; i < points.Length; i++) {
-                        float pointTime = i / HZ;
-                        if (pointTime >= targetStartTime) {
-                            startIndex = i;
-                            break;
+                    int left = 0;
+                    int right = points.Length - 1;
+
+                    while (left <= right) {
+                        int mid = left + (right - left) / 2;
+                        float pointTime = mid / HZ;
+
+                        if (pointTime < targetStartTime) {
+                            left = mid + 1;
+                        }
+                        else {
+                            startIndex = mid;
+                            right = mid - 1;
                         }
                     }
                 }
 
                 if (segment.EndTime < float.MaxValue) {
                     float targetEndTime = segment.EndTime;
-                    for (int i = startIndex; i < points.Length; i++) {
-                        float pointTime = i / HZ;
-                        if (pointTime >= targetEndTime) {
-                            endIndex = math.max(startIndex + 1, i);
-                            break;
+                    int left = startIndex;
+                    int right = points.Length - 1;
+
+                    while (left <= right) {
+                        int mid = left + (right - left) / 2;
+                        float pointTime = mid / HZ;
+
+                        if (pointTime < targetEndTime) {
+                            left = mid + 1;
+                        }
+                        else {
+                            endIndex = mid;
+                            right = mid - 1;
                         }
                     }
+
+                    endIndex = math.max(startIndex + 1, endIndex);
                 }
 
                 return (startIndex, endIndex);
